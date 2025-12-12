0f4867ffbff4f4045bfac11d173cc8431421da53
Sanket Arun Mule <sanketmu@cybage.com>
2025-12-12 12:44:10 +0530

    final code fixes for parent-auth-service

--- application-backend/parent-auth-service/src/main/java/com/school/management/parentauth/ParentAuthServiceApplication.java
+++ application-backend/parent-auth-service/src/main/java/com/school/management/parentauth/ParentAuthServiceApplication.java
@@ -2,10 +2,10 @@ package com.school.management.parentauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { FlywayAutoConfiguration.class })
 public class ParentAuthServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ParentAuthServiceApplication.class, args);
     }
